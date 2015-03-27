f992e4962e6c87fcda8e9ea545e3136b8183e675
KEJUN JIANG <k4jiang@uwaterloo.ca>
2015-03-27 16:43:46 +0800

    db named incorrectly

--- myapp/db/migrate/20150326043221_create_users.rb
+++ myapp/db/migrate/20150326043221_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers <ActiveRecord::Migration
     def change
       create_table :users do |t|
-        t.string    :user_name,                :null => false, :default => ''
+        t.string    :name,                :null => false, :default => ''
         t.string    :email,               :null => false
         t.string    :crypted_password,    :null => false
         t.string    :password_salt,       :null => false
