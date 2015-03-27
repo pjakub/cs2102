45dbbb0f498b7255538eb712d7ccc4076bf1a185
Peter Jakubowski <p2jakubo@uwaterloo.ca>
2015-03-27 16:22:19 +0800

    -Controller for Article -Model for Article -View for Article 	-Delete Article 	-Edit Article 	-Create Article 	-View Article

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  root 'welcome#index'
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +62,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+  #end
