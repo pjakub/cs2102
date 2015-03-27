479a5d2b19a113356f6651a92b14c00325307fab
KEJUN JIANG <k4jiang@uwaterloo.ca>
2015-03-27 15:47:40 +0800

    Fixed some routes

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :user_sessions
+  resources :'user_sessions'
 
   get 'login' => "user_sessions#new",      :as => :login
   get 'logout' => "user_sessions#destroy", :as => :logout
